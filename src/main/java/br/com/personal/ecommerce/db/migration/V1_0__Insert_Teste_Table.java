2aa2f6b97dc156aa7cd7ea581c4dc74c846cfe97
paulo.reis <paulo.reis@ciandt.com>
2022-03-29 14:04:46 -0300

    Add flyway migration.

---
+++ src/main/java/br/com/personal/ecommerce/db/migration/V1_0__Insert_Teste_Table.java
@@ -0,0 +1,14 @@
+package br.com.personal.ecommerce.db.migration;
+
+import org.flywaydb.core.api.migration.BaseJavaMigration;
+import org.flywaydb.core.api.migration.Context;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+
+public class V1_0__Insert_Teste_Table extends BaseJavaMigration {
+    @Override
+    public void migrate(Context context) throws Exception {
+        new JdbcTemplate(new SingleConnectionDataSource(context.getConnection(), true))
+                .execute("insert into Teste(id, nome) values('1','teste1')");
+    }
+}
